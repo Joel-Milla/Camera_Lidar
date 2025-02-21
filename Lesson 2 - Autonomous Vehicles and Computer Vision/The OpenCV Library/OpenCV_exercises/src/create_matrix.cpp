185789f31da4e02484d064b00418ed84991a1cb5
Joel-Milla <jalejandro.milla@outlook.com>
2025-02-21 12:27:56 +0800

    Crete first image with opencv c++

--- Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
+++ Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
@@ -10,18 +10,22 @@ void createMatrix1()
     // create matrix
     int nrows = 480, ncols = 640;
     cv::Mat m1_8u;
-    m1_8u.create(nrows, ncols, CV_8UC1); // two-channel matrix with 8bit unsigned elements
-    m1_8u.setTo(cv::Scalar(255));        // white
+    m1_8u.create(nrows, ncols, CV_8UC1); // two-channel matrix with 8bit unsigned elements. Has nrows, ncols, and each element is cv with 8bit (0-255 numbers) unsigned (only positive) char, with 1 depth (menaing one color)
+    m1_8u.setTo(cv::Scalar(255)); // Set all colors to 255, which is white
 
     // STUDENT TASK :
     // Create a variable of type cv::Mat* named m3_8u which has three channels with a
+    cv::Mat m3_8u;
+    m3_8u.create(nrows, ncols, CV_8UC3); // now with three channels
     // depth of 8bit per channel. Then, set the first channel to 255 and display the result.
+    m3_8u.setTo(cv::Scalar(255,0,0));
 
 
     // show result
     string windowName = "First steps in OpenCV (m1_8u)";
     cv::namedWindow(windowName, 1); // create window
-    cv::imshow(windowName, m1_8u);
+    // cv::imshow(windowName, m1_8u);
+    cv::imshow(windowName, m3_8u);
     cv::waitKey(0); // wait for keyboard input before continuing
 
     // STUDENT TASK :
