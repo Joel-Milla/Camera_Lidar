99982df8ca6387ad8b6da96c0988ccdb0e992913
Joel-Milla <jalejandro.milla@outlook.com>
2025-03-25 00:04:47 +0800

    Add comments and improve implementaiton of different keypoint matching algorithms.

--- Lesson 4 - Tracking Image Features/Descriptor Matching/descriptor_matching/src/descriptor_matching.cpp
+++ Lesson 4 - Tracking Image Features/Descriptor Matching/descriptor_matching/src/descriptor_matching.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <numeric>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -9,55 +8,65 @@
 
 using namespace std;
 
-void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> &kPtsSource, vector<cv::KeyPoint> &kPtsRef, cv::Mat &descSource, cv::Mat &descRef,
-                      vector<cv::DMatch> &matches, string descriptorType, string matcherType, string selectorType)
-{
+/**
+ * @brief Match keypoints
+ * 
+ * @param imgSource 
+ * @param imgRef 
+ * @param kPtsSource 
+ * @param kPtsRef 
+ * @param descSource 
+ * @param descRef 
+ * @param matches 
+ * @param descriptorType 
+ * @param matcherType 
+ * @param selectorType 
+ * @param crossCheck 
+ */
+void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> &kPtsSource, vector<cv::KeyPoint> &kPtsRef, cv::Mat &descSource, cv::Mat &descRef, vector<cv::DMatch> &matches, string descriptorType, string matcherType, string selectorType, bool crossCheck) {
+    // crosscheck is for computing matches of keys1 to img2 and viceversa. and just match the ones that both have the same match. cannot be activated with KNN because it only works with normal match. Both BF/Flann happen at the same time. BF + crossCheck only keep match if both are mutual best match. Crosscheck + knn cannot be applied because knn fundamentally returns multiple values
 
-    // configure matcher
-    bool crossCheck = false; // crosscheck is for computing matches of keys1 to img2 and viceversa. and just match the ones that both have the same match. cannot be activated with KNN because it only works with normal match
     cv::Ptr<cv::DescriptorMatcher> matcher; // general matcher. tell if use brute force matcher or flann (kd tree) match
 
-    if (matcherType.compare("MAT_BF") == 0)
-    {
+    // Brute force approach where compare all keypoints and compares all distances
+    if (matcherType.compare("MAT_BF") == 0) {
 
         int normType = descriptorType.compare("DES_BINARY") == 0 ? cv::NORM_HAMMING : cv::NORM_L2;
         matcher = cv::BFMatcher::create(normType, crossCheck);
         cout << "BF matching cross-check=" << crossCheck;
     }
-    else if (matcherType.compare("MAT_FLANN") == 0)
-    {
-        if (descSource.type() != CV_32F)
-        { // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
+    // Constructs a kdTree to traverse all distances and returns the matching pairs
+    else if (matcherType.compare("") == 0) {
+        if (descSource.type() != CV_32F) { 
+            // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
             descSource.convertTo(descSource, CV_32F);
             descRef.convertTo(descRef, CV_32F);
         }
 
-        // TODO : implement FLANN matching
+        //* Implement FLANN matching
         matcher = cv::FlannBasedMatcher::create();
         cout << "FLANN matching";
     }
 
     // perform matching task
-    // in here we tell if want nearest neighbor (to keep only best match) or knn (keeping best k keypoints per match) and then computing the ratio between all of them and at the end choose the best one. this helps reduce ambiguos points where some keypoints could be very similar, reduces FP. 
-    if (selectorType.compare("SEL_NN") == 0)
-    { // nearest neighbor (best match)
-
+    // in here we tell if want nearest neighbor (to keep only best match) or knn (keeping best k keypoints per match and then compute distnace ratio to know if discard it or not). Using implementation above, if chooose Bf then compute all distances and then choose best. If choose flann, then that returns closes above certain threshold but faster than BF
+    if (selectorType.compare("SEL_NN") == 0) { 
+        // nearest neighbor (best match)
         double t = (double)cv::getTickCount();
         matcher->match(descSource, descRef, matches); // Finds the best match for each descriptor in desc1
         t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
         cout << " (NN) with n=" << matches.size() << " matches in " << 1000 * t / 1.0 << " ms" << endl;
     }
-    else if (selectorType.compare("SEL_KNN") == 0)
-    { // k nearest neighbors (k=2)
-
+    // knn (keeping best k keypoints per match) and then computing the ratio between all of them and at the end choose the best one. this helps reduce ambiguos points where some keypoints could be very similar, reduces FP. If BF, then compute all distances and from list returned then performed Knn where compared the first two closest and compare distance ratio. For each match, it returns the two closes. Our work is compute disntace ratio and decide which to show
+    else if (selectorType.compare("SEL_KNN") == 0) {
         double t = (double)cv::getTickCount();
-        // TODO : implement k-nearest-neighbor matching
+        //* k-nearest-neighbor matching
         int k = 2;
         vector<vector<cv::DMatch>> knnMatches;
         matcher->knnMatch(descSource, descRef, knnMatches, 2);
 
 
-        // TODO : filter matches using descriptor distance ratio test
+        //* Filter matches using descriptor distance ratio test
         float threshold = 0.8;
         int discarded = 0;
         for (int indx = 0; indx < knnMatches.size(); indx++) {
@@ -66,9 +75,9 @@ void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> 
             float firstDist = match[0].distance;
             float secondDist = match[1].distance;
             float ratio = firstDist / secondDist;
-            bool valid_number = firstDist < threshold * secondDist;
+            bool valid_match = ratio < threshold;
 
-            if (valid_number)
+            if (valid_match)
                 matches.push_back(match[0]);
             else
                 discarded++;
@@ -89,73 +98,78 @@ void matchDescriptors(cv::Mat &imgSource, cv::Mat &imgRef, vector<cv::KeyPoint> 
     cv::namedWindow(windowName, 7);
     cv::imshow(windowName, matchImg);
     cv::waitKey(0);
+    
+    cout << endl;
 }
 
-/*
-RESULTS
-1) Brute force + Nearest neighboors
--BF matching cross-check=0 (NN) with n=2896 matches in 14.6868 ms
--BF matching cross-check=1 (NN) with n=1705 matches in 23.1417 ms
-
--Takes longer in general but less matches made.
-
-2) Brute force + KNearest - in two datasets
-- BF matching cross-check=0 (KNN) with n=1578 matches in 19.8127 ms 
-1318 discarded points
-- BF matching cross-check=0 (KNN) with n=55 matches in 3.02166 ms 
-45 discarded points
-
-Rougly half of the points where discarded
-
-3) BF vs Flann
-** BRISK - USING HAMMING BECAUS BINARY
-- FLANN matching (NN) with n=2896 matches in 61.0417 ms
-- FLANN matching (KNN) with n=1177 matches in 45.4164 ms 
-1719 discarded points
-
-** SIFT - USING L2 NORM
-- FLANN matching (NN) with n=2896 matches in 45.4864 ms
-- FLANN matching (KNN) with n=1177 matches in 45.5844 ms 
-1719 discarded points
-
-- BF matching cross-check=0 (NN) with n=1890 matches in 15.1509 ms
-
-In general flann is much sower than previous algorihtms, taking more miliseconds to compute
-*/
-
-int main()
-{
+/**
+ * @brief Recieves parameters and files to read, that will be used when calling matchDescriptor (to match keypoints)
+ * 
+ * @param brisk Use brisk dataset?
+ * @param small Use small dataset?
+ * @param crossCheck Use crosscheck to discard keypoints? when not using knn
+ * @param descriptorType Using binary descriptors or HOG for sift
+ * @param matcherType Using flann or brute force approach
+ * @param selectorType Using NN (nearest neighbor) to choose keypoints or kNN to discard similar keypoints
+ */
+void callMethod(bool brisk, bool small, bool crossCheck, string descriptorType, string matcherType, string selectorType) {
     cv::Mat imgSource = cv::imread("../images/img1gray.png");
     cv::Mat imgRef = cv::imread("../images/img2gray.png");
 
-    // vector<cv::KeyPoint> kptsSource, kptsRef; 
-    // readKeypoints("../dat/C35A5_KptsSource_BRISK_large.dat", kptsSource);
-    // readKeypoints("../dat/C35A5_KptsRef_BRISK_large.dat", kptsRef);
-
-    // cv::Mat descSource, descRef; 
-    // readDescriptors("../dat/C35A5_DescSource_BRISK_large.dat", descSource);
-    // readDescriptors("../dat/C35A5_DescRef_BRISK_large.dat", descRef);
-    
-    // vector<cv::KeyPoint> kptsSource, kptsRef; 
-    // readKeypoints("../dat/C35A5_KptsSource_BRISK_small.dat", kptsSource);
-    // readKeypoints("../dat/C35A5_KptsRef_BRISK_small.dat", kptsRef);
-
-    // cv::Mat descSource, descRef; 
-    // readDescriptors("../dat/C35A5_DescSource_BRISK_small.dat", descSource);
-    // readDescriptors("../dat/C35A5_DescRef_BRISK_small.dat", descRef);
-
     vector<cv::KeyPoint> kptsSource, kptsRef; 
-    readKeypoints("../dat/C35A5_KptsSource_SIFT.dat", kptsSource);
-    readKeypoints("../dat/C35A5_KptsRef_SIFT.dat", kptsRef);
+    cv::Mat descSource, descRef;
+    string dataset;
 
-    cv::Mat descSource, descRef; 
-    readDescriptors("../dat/C35A5_DescSource_SIFT.dat", descSource);
-    readDescriptors("../dat/C35A5_DescRef_SIFT.dat", descRef);
+    if (brisk && !small) {
+        dataset = "Brisk large";
+        readKeypoints("../dat/C35A5_KptsSource_BRISK_large.dat", kptsSource);
+        readKeypoints("../dat/C35A5_KptsRef_BRISK_large.dat", kptsRef);
+
+        readDescriptors("../dat/C35A5_DescSource_BRISK_large.dat", descSource);
+        readDescriptors("../dat/C35A5_DescRef_BRISK_large.dat", descRef);
+    }
+
+    if (brisk && small) {
+        dataset = "Brisk small";
+        readKeypoints("../dat/C35A5_KptsSource_BRISK_small.dat", kptsSource);
+        readKeypoints("../dat/C35A5_KptsRef_BRISK_small.dat", kptsRef);
+
+        readDescriptors("../dat/C35A5_DescSource_BRISK_small.dat", descSource);
+        readDescriptors("../dat/C35A5_DescRef_BRISK_small.dat", descRef);
+    }    
+
+    if (!brisk) {
+        dataset = "Sift";
+        readKeypoints("../dat/C35A5_KptsSource_SIFT.dat", kptsSource);
+        readKeypoints("../dat/C35A5_KptsRef_SIFT.dat", kptsRef);
+        
+        readDescriptors("../dat/C35A5_DescSource_SIFT.dat", descSource);
+        readDescriptors("../dat/C35A5_DescRef_SIFT.dat", descRef);
+    }
 
 
     vector<cv::DMatch> matches;
-    string matcherType = "MAT_BF"; 
-    string descriptorType = 0 ? "DES_BINARY" : ""; 
-    string selectorType = "SEL_NN";
-    matchDescriptors(imgSource, imgRef, kptsSource, kptsRef, descSource, descRef, matches, descriptorType, matcherType, selectorType);
+    cout << dataset << endl;
+    matchDescriptors(imgSource, imgRef, kptsSource, kptsRef, descSource, descRef, matches, descriptorType, matcherType, selectorType, crossCheck);
+}
+
+int main() {
+
+    //* First task compare brisk small with corssCheck turned off vs on
+    callMethod(true, true, false, "DES_BINARY", "MAT_BF", "SEL_NN");
+
+    callMethod(true, true, true, "DES_BINARY", "MAT_BF", "SEL_NN");
+
+    //* Second task, brisk small vs large using KNN
+    callMethod(true, true, false, "DES_BINARY", "MAT_BF", "SEL_KNN");
+
+    callMethod(true, false, false, "DES_BINARY", "MAT_BF", "SEL_NN");
+    callMethod(true, false, false, "DES_BINARY", "MAT_BF", "SEL_KNN");
+
+    //* Third task, use brute force vs flann on brisk large and sift
+    callMethod(true, false, false, "DES_BINARY", "", "SEL_KNN");
+
+    callMethod(false, false, false, "", "MAT_BF", "SEL_KNN");
+
+    callMethod(false, false, false, "", "", "SEL_KNN");
 }
